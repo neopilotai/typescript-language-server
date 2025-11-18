85e660f35f3e1f8053efb364a8fe13dd12ecc1d4
NeoPilot <221231603+neopilotai@users.noreply.github.com>
2025-11-18 06:44:18 +0600

    Add support for websockets - treat language server as WS server

--- src/cli.ts
+++ src/cli.ts
@@ -5,28 +5,48 @@
  * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
  */
 
-import { readFileSync } from 'node:fs';
+// import { readFileSync } from 'node:fs';
 import { Command } from 'commander';
-import lsp from 'vscode-languageserver';
+import lsp from 'vscode-languageserver/node.js';
+import { WebSocketMessageReader, WebSocketMessageWriter, toSocket } from 'vscode-ws-jsonrpc';
 import { createLspConnection } from './lsp-connection.js';
+import * as ws from 'ws';
 
 const DEFAULT_LOG_LEVEL = lsp.MessageType.Info;
-const { version } = JSON.parse(readFileSync(new URL('../package.json', import.meta.url), { encoding: 'utf8' })) as { version: string; };
+// const { version } = JSON.parse(readFileSync(new URL('../package.json', import.meta.url), { encoding: 'utf8' }));
 
 const program = new Command('typescript-language-server')
-    .version(version)
-    .requiredOption('--stdio', 'use stdio')
-    .option<number>('--log-level <logLevel>', 'A number indicating the log level (4 = log, 3 = info, 2 = warn, 1 = error). Defaults to `2`.', value => parseInt(value, 10), 2)
+    // .version(version)
+    // .requiredOption('--stdio', 'use stdio')
+    .option('--log-level <logLevel>', 'A number indicating the log level (4 = log, 3 = info, 2 = warn, 1 = error). Defaults to `2`.')
     .parse(process.argv);
 
-const options = program.opts<{ logLevel: number; }>();
+const options = program.opts();
 
 let logLevel = DEFAULT_LOG_LEVEL;
-if (options.logLevel && (options.logLevel < 1 || options.logLevel > 4)) {
-    console.error(`Invalid '--log-level ${logLevel}'. Falling back to 'info' level.`);
-    logLevel = DEFAULT_LOG_LEVEL;
+if (options.logLevel) {
+    logLevel = parseInt(options.logLevel, 10);
+    if (logLevel && (logLevel < 1 || logLevel > 4)) {
+        console.error(`Invalid '--log-level ${logLevel}'. Falling back to 'info' level.`);
+        logLevel = DEFAULT_LOG_LEVEL;
+    }
 }
 
-createLspConnection({
-    showMessageLevel: logLevel as lsp.MessageType,
-}).listen();
+const port = 9090;
+const wss = new ws.WebSocketServer({ port });
+
+let connection: lsp.Connection | undefined = undefined;
+
+wss.on('connection', (socket) => {
+    if (connection) {
+        connection.dispose();
+    }
+    const sock = toSocket(socket);
+    const reader = new WebSocketMessageReader(sock);
+    const writer = new WebSocketMessageWriter(sock);
+    connection = lsp.createConnection(lsp.ProposedFeatures.all, reader, writer);
+
+    createLspConnection({
+        showMessageLevel: logLevel as lsp.MessageType,
+    }, connection).listen();
+});
